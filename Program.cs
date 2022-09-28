8d9edaa0a5a136955f12b8ffb9e9a5f5ad2d7275
sibergold <kaynakca21@hotmail.com>
2022-09-28 17:31:12 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace algoritma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cümleni gir.");
+            string cumle  = Console.ReadLine();
+            string cumle2="";
+            string[] kelimeler=cumle.Split(',');
+            for(int i=0;i<kelimeler.Length;i++)
+            {
+                cumle2+=kelimeler[0];
+                for(int j=1;j<kelimeler.Length;j++)
+                {
+                    if(kelimeler[j].Contains(Convert.ToString(j)))
+                    {
+                         kelimeler[j].Remove(j);
+                    }
+                else
+                {
+                    continue;
+                }
+                }
+            }
+           Console.WriteLine(cumle2);
+           
+        }
+    }
+}
